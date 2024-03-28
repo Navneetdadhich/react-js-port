68727d4311cc51c800105f5b805b58b5f27551b6
Nvntdad <nvntdadhich@gmail.com>
2024-03-29 01:57:00 +0530

    Initial commit

---
+++ project-1/src/components/hero.jsx
@@ -0,0 +1,40 @@
+const Hero = () => {
+    return <> 
+ 
+    <div className="hero-sec">
+        <div>
+                <div className="hero-title">
+                    <h1>YOUR FEET <br /> DESERVE <br />
+                    THE BEST
+                    </h1>
+                </div>
+
+                <p className="para">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                    Voluptate tempora obcaecati quidem illo voluptatum quae 
+                    officiis eligendi eos temporibus perspiciatis nam, quos 
+                     Numquam, maxime ea.
+                </p>
+
+                <div className="buttons-hero">
+                    <button className="b1">Shop Now</button>
+                    <button className="b2">Catogory</button>
+                </div>
+
+                <p>Also Available On : </p>
+                <div className="online-images">
+                    <img src="/images/amazon.png" alt="amazon-logo" />
+                    <img src="/images/flipkart.png" alt="flipkart-logo" />
+                </div>
+        </div>
+
+        <div className="photo">
+                <img src="/images/shoes.png" alt="shoes" />
+        </div>
+
+    </div>
+
+    </>
+}
+
+export default Hero;
---
+++ project-1/src/components/nav.jsx
@@ -0,0 +1,19 @@
+const Navigation = () => {
+return  <nav>
+<div className="logo">
+  <img src="/images/nike.png" alt="logo" />
+</div>
+
+<ul>
+  <li href="#">menu</li>
+  <li href="#">location</li>
+  <li href="#">about</li>
+  <li href="#">trends</li>
+</ul>
+
+<button>login</button>
+</nav>
+
+}
+
+export default Navigation;
